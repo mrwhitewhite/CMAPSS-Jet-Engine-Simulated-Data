b3a3187fb95880c82fcf116d2daaa8e74c57ecfc
mrwhitewhite <lowmingshun@gmail.com>
2026-03-10 14:00:29 +0800

    scripts: fix dataset wrong RUL

--- scripts/00 Process data.py
+++ scripts/00 Process data.py
@@ -47,24 +47,41 @@ RUL_1 = df_ls[0]
 RUL_2 = df_ls[1]
 RUL_3 = df_ls[2]
 RUL_4 = df_ls[3]
+
+RUL_1 = RUL_1.rename({"RUL": "MAX_RUL"})
+RUL_2 = RUL_2.rename({"RUL": "MAX_RUL"})
+RUL_3 = RUL_3.rename({"RUL": "MAX_RUL"})
+RUL_4 = RUL_4.rename({"RUL": "MAX_RUL"})
+
 test_1 = df_ls[4]
 test_2 = df_ls[5]
 test_3 = df_ls[6]
 test_4 = df_ls[7]
+
 train_1 = df_ls[8]
 train_2 = df_ls[9]
 train_3 = df_ls[10]
 train_4 = df_ls[11]
 
-train_1 = train_1.with_columns(pl.col("CYCLE").max().over("UNIT_NUM").alias("RUL"))
-train_2 = train_2.with_columns(pl.col("CYCLE").max().over("UNIT_NUM").alias("RUL"))
-train_3 = train_3.with_columns(pl.col("CYCLE").max().over("UNIT_NUM").alias("RUL"))
-train_4 = train_4.with_columns(pl.col("CYCLE").max().over("UNIT_NUM").alias("RUL"))
+train_1 = train_1.with_columns(pl.col("CYCLE").max().over("UNIT_NUM").alias("MAX_RUL"))
+train_2 = train_2.with_columns(pl.col("CYCLE").max().over("UNIT_NUM").alias("MAX_RUL"))
+train_3 = train_3.with_columns(pl.col("CYCLE").max().over("UNIT_NUM").alias("MAX_RUL"))
+train_4 = train_4.with_columns(pl.col("CYCLE").max().over("UNIT_NUM").alias("MAX_RUL"))
 
 test_1 = test_1.join(RUL_1, on="UNIT_NUM", how="left")
 test_2 = test_2.join(RUL_2, on="UNIT_NUM", how="left")
 test_3 = test_3.join(RUL_3, on="UNIT_NUM", how="left")
 test_4 = test_4.join(RUL_4, on="UNIT_NUM", how="left")
+
+train_1 = train_1.with_columns((pl.col("MAX_RUL") - pl.col("CYCLE")).alias("RUL"))
+train_2 = train_2.with_columns((pl.col("MAX_RUL") - pl.col("CYCLE")).alias("RUL"))
+train_3 = train_3.with_columns((pl.col("MAX_RUL") - pl.col("CYCLE")).alias("RUL"))
+train_4 = train_4.with_columns((pl.col("MAX_RUL") - pl.col("CYCLE")).alias("RUL"))
+
+test_1 = test_1.with_columns((pl.col("MAX_RUL") - pl.col("CYCLE")).alias("RUL"))
+test_2 = test_2.with_columns((pl.col("MAX_RUL") - pl.col("CYCLE")).alias("RUL"))
+test_3 = test_3.with_columns((pl.col("MAX_RUL") - pl.col("CYCLE")).alias("RUL"))
+test_4 = test_4.with_columns((pl.col("MAX_RUL") - pl.col("CYCLE")).alias("RUL"))
 
 train = pl.concat([train_1, train_2, train_3, train_4])
 test = pl.concat([test_1, test_2, test_3, test_4])
